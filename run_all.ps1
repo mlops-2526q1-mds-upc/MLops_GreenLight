d1f7a1757a23eb1acfabc6690e83069e698b8e5a
brunabarraquer <brunabarraquer@gmail.com>
2025-10-08 10:45:49 +0200

    dvc

--- run_all.ps1
+++ run_all.ps1
@@ -26,7 +26,7 @@ if ([string]::IsNullOrEmpty($IMAGE_EXISTS)) {
 # ------------------------------
 $CONTAINER_COMMANDS = @"
 echo "Running all commands..."
-python3 ./mlops_greenlight/dataset.py
+python3 ./mlops_greenlight/dataset.pymkdir .dvcstore
 python3 ./mlops_greenlight/modeling/train.py
 python3 ./mlops_greenlight/modeling/test.py
 bash ./mlops_greenlight/predictions2mp4.sh ./models/predictions/ ./models/predictions/output.mp4
@@ -47,3 +47,4 @@ docker run --rm `
   -w $CONTAINER_WORKDIR `
   $IMAGE_NAME `
   bash $TMP_SCRIPT
+
